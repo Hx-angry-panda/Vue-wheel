456db77e384d4d89b09a0470e11aec4963170c07
AngryPanda <924193579guge@gmail.com>
2020-01-26 18:27:00 +0800

    验证 iconPosition 为 left || right

--- src/button.vue
+++ src/button.vue
@@ -10,7 +10,16 @@
 </template>
 <script>
     export default {
-        props: ['icon', 'icon-position']
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator (value) {
+                    return value === 'left' || value === 'right'
+                }
+            }
+        }
     }
 </script>
 <style lang="scss">
