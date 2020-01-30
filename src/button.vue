916d6828e72652ce34df84067798d251e5b64be6
AngryPanda <924193579guge@gmail.com>
2020-01-30 17:12:16 +0800

    添加 Loading 的 click 事件

--- src/button.vue
+++ src/button.vue
@@ -1,7 +1,7 @@
 <template>
-    <button class="g-button" v-bind:class="{[`icon-${iconPosition}`]: true}">
-        <g-icon v-if="icon" v-bind:name="icon" class="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+    <button class="g-button" v-bind:class="{[`icon-${iconPosition}`]: true}" @click="$emit('click')">
+        <g-icon v-if="icon && !loading" v-bind:name="icon" class="icon"></g-icon>
+        <g-icon class="loading icon" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
